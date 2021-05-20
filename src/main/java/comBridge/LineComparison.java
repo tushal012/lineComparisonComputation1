0af685d404c18258b9ac4cb02e231d5ecf27c018
tushal012 <tushalkashyap11@gmail.com>
2021-05-20 21:48:15 +0530

    Added: Calculate its length

--- src/main/java/comBridge/LineComparison.java
+++ src/main/java/comBridge/LineComparison.java
@@ -4,5 +4,12 @@ public class LineComparison {
 
     public static void main(String[] args) {
         System.out.println("Welcome to line comparison computation");
+
+        int x1,x2,y1,y2;
+        double dis;
+        x1=1;y1=1;x2=4;y2=4;
+        dis=Math.sqrt((x2-x1)^2 + (y2-y1)^2);
+        System.out.println("distinctive"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis);
+    }
     }
 }
